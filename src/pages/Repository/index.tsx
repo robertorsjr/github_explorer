8ce2d95fea0a2f8dca162fed1db71f9d8c9071f6
robertorsjr <beto.rodriguesds@gmail.com>
2021-03-16 02:06:30 -0300

    routes awork

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Repository: React.FC = () => {
+  return (
+    <h1>Repository</h1>
+  );
+}
+
+export default Repository;
---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Switch, Route } from  'react-router-dom'
+import { Dashboard, Repository } from '../pages'
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" component={Dashboard} exact/>
+    <Route path="/repository" component={Repository}/>
+  </Switch>
+)
+
+export default Routes;
+
+
