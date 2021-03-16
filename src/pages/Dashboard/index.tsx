d911dd5a65c0669e8ffa2e84e754959ea19cf247
robertorsjr <beto.rodriguesds@gmail.com>
2021-03-16 13:36:43 -0300

    error handling on the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, FormEvent } from 'react';
 import { Repositories } from '../../components'
 import api from '../../services';
-import { Container, Title, Logo, Form } from './styles';
+import { Container, Title, Logo, Form, Error } from './styles';
 
 interface Repository{
   full_name: string;
@@ -14,28 +14,42 @@ interface Repository{
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
   const [repositories, setRepositories] = useState<Repository[]>([])
  
   async function hendleAddRepository(event: FormEvent<HTMLFormElement>){
     event.preventDefault();
+    if(!newRepo){
+      setInputError('Digite o autor/nome do repositório')
+      return;
+    }
+
+    try{
     const response = await api.get<Repository>(`repos/${newRepo}`)
-    console.warn(response.data)
     const repository = response.data;
     setRepositories([...repositories , repository]);
+    setInputError('')
+
+    }catch(err){
+      setInputError('Erro na busca por esse repositório')
+    }
   }
 
   return(
     <Container>
       <Logo/>
       <Title>Explore repositórios no Github.</Title>
-      <Form onSubmit={hendleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={hendleAddRepository}>
         <input
-        value={newRepo}
-        onChange={(e)=> setNewRepo(e.target.value)} 
-        placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e)=> setNewRepo(e.target.value)} 
+          placeholder="Digite o nome do repositório"
         />
         <button type="submit">pesquisar</button>
-      </Form> 
+      </Form>
+      { 
+        inputError && <Error>{inputError}</Error> 
+      }
       {
         repositories.map(repository =>
           <Repositories 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import logo from '../../assets/logo.svg'
 import { shade } from 'polished'
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Container = styled.div`
   
@@ -20,7 +24,7 @@ export const Title = styled.div`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display:flex;
@@ -32,6 +36,12 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px; 
     color:#3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${(props)=> props.hasError && css`
+      border-color: #c53030;
+    `}
 
     &::placeholder{
       color: #a8a8b3;
@@ -52,3 +62,9 @@ export const Form = styled.form`
     }
   }
 `;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`
